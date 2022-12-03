f20cd2f2ee5fa875463bf887d6a277618257cd41
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-12-03 16:07:16 -0300

    feat: ensure CreateUserController calls CreateUser usecase with the correct values

--- src/presentation/controllers/create/create-user-controller.ts
+++ src/presentation/controllers/create/create-user-controller.ts
@@ -1,10 +1,17 @@
+import { ICreateUser } from "../../../domain/usecases/create-user";
 import { InvalidParamError } from "../errors/invalid-param-error";
 import { MissingParamError } from "../errors/missing-param-error";
-import { badRequest } from "../helpers/http";
+import { badRequest, ok } from "../helpers/http";
 import { IController } from "../protocols/controller";
 import { HttpRequest, HttpResponse } from "../protocols/http";
 
 export class CreateUserController implements IController {
+	private readonly createUser: ICreateUser;
+
+	constructor(createUser: ICreateUser) {
+		this.createUser = createUser;
+	}
+
 	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
 		const requiredFields = [
 			"nome",
@@ -14,6 +21,7 @@ export class CreateUserController implements IController {
 			"sexo",
 			"dataNascimento",
 		];
+
 		for (const field of requiredFields) {
 			if (!httpRequest.body[field]) {
 				return badRequest(new MissingParamError(field));
@@ -21,8 +29,8 @@ export class CreateUserController implements IController {
 		}
 
 		if (
-			httpRequest.body["sexo"] !== "Masculino" ||
-			httpRequest.body["sexo"] !== "Feminino" ||
+			httpRequest.body["sexo"] !== "Masculino" &&
+			httpRequest.body["sexo"] !== "Feminino" &&
 			httpRequest.body["sexo"] !== "Outro"
 		) {
 			return badRequest(
@@ -30,6 +38,8 @@ export class CreateUserController implements IController {
 			);
 		}
 
-		throw new Error("Method not implemented.");
+		await this.createUser.create(httpRequest.body);
+
+		return ok("");
 	}
 }
