37180b55a0e980a664bf416ca14376d3b0c24b5b
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-12-03 15:15:19 -0300

    refactor: move sut creation to helper methods

--- src/application/usecases/create-user.spec.ts
+++ src/application/usecases/create-user.spec.ts
@@ -3,41 +3,57 @@ import { CreateUserModel } from "../../domain/usecases/create-user";
 import { IAddUserRepository } from "../protocols/add-user-respository";
 import { CreateUser } from "./create-user";
 
+const makeFakeCreateUserModel = (): CreateUserModel => {
+	return {
+		nome: "nome_valido",
+		CPF: "CPF_valido",
+		email: "email_valido",
+		telefone: "telefone_valido",
+		sexo: "Masculino",
+		dataNascimento: "15/10/1980",
+	};
+};
+
+const makeFakeUserModel = (): UserModel => {
+	return {
+		id: "id_valido",
+		nome: "nome_valido",
+		CPF: "CPF_valido",
+		email: "email_valido",
+		telefone: "telefone_valido",
+		sexo: "Masculino",
+		dataNascimento: "15/10/1980",
+	};
+};
+
+const makeAddUserRepositoryStub = (): IAddUserRepository => {
+	class AddUserRepositoryStub implements IAddUserRepository {
+		async add(createUserData: CreateUserModel): Promise<UserModel> {
+			return new Promise((resolve) => resolve(makeFakeUserModel()));
+		}
+	}
+	return new AddUserRepositoryStub();
+};
+
+type SutTypes = {
+	sut: CreateUser;
+	addUserRepositoryStub: IAddUserRepository;
+};
+
+const makeSut = (): SutTypes => {
+	const addUserRepositoryStub = makeAddUserRepositoryStub();
+	const sut = new CreateUser(addUserRepositoryStub);
+	return {
+		sut,
+		addUserRepositoryStub,
+	};
+};
+
 describe("Create User Usecase", () => {
 	test("Should call AddUserRepository with the correct values", () => {
-		class AddUserRepositoryStub implements IAddUserRepository {
-			async add(createUserData: CreateUserModel): Promise<UserModel> {
-				return new Promise((resolve) =>
-					resolve({
-						id: "id_valido",
-						nome: "nome_valido",
-						CPF: "CPF_valido",
-						email: "email_valido",
-						telefone: "telefone_valido",
-						sexo: "Masculino",
-						dataNascimento: "15/10/1980",
-					})
-				);
-			}
-		}
-		const addUserRepositoryStub = new AddUserRepositoryStub();
-		const sut = new CreateUser(addUserRepositoryStub);
+		const { sut, addUserRepositoryStub } = makeSut();
 		const addUserRepositorySpy = jest.spyOn(addUserRepositoryStub, "add");
-		sut.create({
-			nome: "nome_valido",
-			CPF: "CPF_valido",
-			email: "email_valido",
-			telefone: "telefone_valido",
-			sexo: "Masculino",
-			dataNascimento: "15/10/1980",
-		});
-		expect(addUserRepositorySpy).toHaveBeenCalledWith({
-			nome: "nome_valido",
-			CPF: "CPF_valido",
-			email: "email_valido",
-			telefone: "telefone_valido",
-			sexo: "Masculino",
-			dataNascimento: "15/10/1980",
-		});
+		sut.create(makeFakeCreateUserModel());
+		expect(addUserRepositorySpy).toHaveBeenCalledWith(makeFakeCreateUserModel());
 	});
 });
