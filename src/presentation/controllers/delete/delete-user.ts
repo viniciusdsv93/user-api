268bb08fe56d496cb39e0761e638172d6aeb2e12
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-12-04 11:43:25 -0300

    feat: ensure DeleteUserController returns 400 if an invalid email is provided

--- src/presentation/controllers/delete/delete-user.ts
+++ src/presentation/controllers/delete/delete-user.ts
@@ -1,4 +1,5 @@
 import { IDeleteUser } from "../../../domain/usecases/delete-user";
+import { InvalidParamError } from "../../errors/invalid-param-error";
 import { MissingParamError } from "../../errors/missing-param-error";
 import { badRequest, ok } from "../../helpers/http";
 import { IController } from "../../protocols/controller";
@@ -14,6 +15,12 @@ export class DeleteUserController implements IController {
 	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
 		if (!httpRequest.params?.email) {
 			return badRequest(new MissingParamError("email"));
+		}
+
+		if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(httpRequest.params.email)) {
+			return badRequest(
+				new InvalidParamError("email", "O email informado é inválido")
+			);
 		}
 
 		return ok("");
