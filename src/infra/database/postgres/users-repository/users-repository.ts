821d781bc0ba869008cd6e84d254963016236f93
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-12-04 00:49:00 -0300

    feat: ensure PrismaUsersRepository returns an user on add success

--- src/infra/database/postgres/users-repository/users-repository.ts
+++ src/infra/database/postgres/users-repository/users-repository.ts
@@ -6,6 +6,8 @@ import { prismaClient } from "../prisma/prisma-client";
 export class UsersPrismaRepository implements IAddUserRepository {
 	async add(createUserData: CreateUserModel): Promise<UserModel> {
 		const { nome, CPF, email, telefone, sexo, dataNascimento } = createUserData;
+		const [day, month, year] = dataNascimento.split("/");
+		const formattedDate = new Date(Number(year), Number(month) - 1, Number(day));
 		return await prismaClient.user.create({
 			data: {
 				nome,
@@ -13,7 +15,7 @@ export class UsersPrismaRepository implements IAddUserRepository {
 				email,
 				telefone,
 				sexo,
-				dataNascimento,
+				dataNascimento: formattedDate,
 			},
 		});
 	}
