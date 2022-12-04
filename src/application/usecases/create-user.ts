1fba2f8aa0d7d750af46abade11db0ab57fcbefa
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-12-04 01:31:20 -0300

    feat: ensure CreateUser calls FindUserByEmail with the correct email

--- src/application/usecases/create-user.ts
+++ src/application/usecases/create-user.ts
@@ -1,15 +1,22 @@
 import { UserModel } from "../../domain/models/user";
 import { CreateUserModel, ICreateUser } from "../../domain/usecases/create-user";
 import { IAddUserRepository } from "../protocols/add-user-respository";
+import { IFindUserByEmailRepository } from "../protocols/find-by-email-repository";
 
 export class CreateUser implements ICreateUser {
 	private readonly addUserRepository: IAddUserRepository;
+	private readonly findUserByEmailRepository: IFindUserByEmailRepository;
 
-	constructor(addUserRepository: IAddUserRepository) {
+	constructor(
+		addUserRepository: IAddUserRepository,
+		findUserByEmailRepository: IFindUserByEmailRepository
+	) {
 		this.addUserRepository = addUserRepository;
+		this.findUserByEmailRepository = findUserByEmailRepository;
 	}
 
 	async create(createUserData: CreateUserModel): Promise<UserModel> {
+		await this.findUserByEmailRepository.findByEmail(createUserData.email);
 		const result = await this.addUserRepository.add(createUserData);
 		return result;
 	}
