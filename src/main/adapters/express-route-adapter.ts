f8e81fa82abda96de1a68c3589e3ad920f6f4b65
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-12-04 00:57:42 -0300

    feat: create express route adapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { IController } from "../../presentation/controllers/protocols/controller";
+import { HttpRequest, HttpResponse } from "../../presentation/controllers/protocols/http";
+
+export const expressAdaptRoute = (controller: IController) => {
+	return async (req: Request, res: Response) => {
+		const httpRequest: HttpRequest = {
+			body: req.body,
+		};
+		const httpResponse: HttpResponse = await controller.handle(httpRequest);
+		if (httpResponse.statusCode === 200 || httpResponse.statusCode === 201) {
+			res.status(httpResponse.statusCode).json(httpResponse.body);
+		} else {
+			res.status(httpResponse.statusCode).json({
+				error: httpResponse.body.message,
+			});
+		}
+	};
+};
