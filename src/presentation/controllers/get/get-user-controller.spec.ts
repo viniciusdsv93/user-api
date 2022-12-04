190a1ab31ee51696a01fa85952472408ccf827de
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-12-04 10:39:04 -0300

    test: ensure GetUserController returns 400 if an invalid email is provided

--- src/presentation/controllers/get/get-user-controller.spec.ts
+++ src/presentation/controllers/get/get-user-controller.spec.ts
@@ -1,3 +1,4 @@
+import { InvalidParamError } from "../../errors/invalid-param-error";
 import { MissingParamError } from "../../errors/missing-param-error";
 import { badRequest } from "../../helpers/http";
 import { GetUserController } from "./get-user-controller";
@@ -9,5 +10,17 @@ describe("Get User Controller", () => {
 			params: {},
 		});
 		expect(httpResponse).toEqual(badRequest(new MissingParamError("email")));
+	});
+
+	test("Should return 400 if an invalid email is provided", async () => {
+		const sut = new GetUserController();
+		const httpResponse = await sut.handle({
+			params: {
+				email: "email_invalido",
+			},
+		});
+		expect(httpResponse).toEqual(
+			badRequest(new InvalidParamError("email", "O email informado é inválido"))
+		);
 	});
 });
