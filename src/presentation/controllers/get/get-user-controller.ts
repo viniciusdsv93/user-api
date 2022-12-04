5f93f4b2e4b9b82fc32f59f4cefd0a0c3fc0a37e
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-12-04 10:39:00 -0300

    feat: ensure GetUserController returns 400 if an invalid email is provided

--- src/presentation/controllers/get/get-user-controller.ts
+++ src/presentation/controllers/get/get-user-controller.ts
@@ -1,3 +1,4 @@
+import { InvalidParamError } from "../../errors/invalid-param-error";
 import { MissingParamError } from "../../errors/missing-param-error";
 import { badRequest, ok } from "../../helpers/http";
 import { IController } from "../../protocols/controller";
@@ -8,6 +9,13 @@ export class GetUserController implements IController {
 		if (!httpRequest.params?.email) {
 			return badRequest(new MissingParamError("email"));
 		}
+
+		if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(httpRequest.params.email)) {
+			return badRequest(
+				new InvalidParamError("email", "O email informado é inválido")
+			);
+		}
+
 		return ok("");
 	}
 }
