a1b4c6a70ca0e75125c55a257e478e8b581bca2e
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-12-04 13:16:59 -0300

    feat: include update controller on routes

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -3,11 +3,13 @@ import { expressAdaptRoute } from "../adapters/express-route-adapter";
 import { makeCreateUserController } from "../factories/create-user";
 import { makeDeleteUserController } from "../factories/delete-user";
 import { makeGetUserController } from "../factories/get-user";
+import { makeUpdateUserController } from "../factories/update-user";
 
 const router = Router();
 
 router.post("/create", expressAdaptRoute(makeCreateUserController()));
 router.get("/get/:email", expressAdaptRoute(makeGetUserController()));
 router.delete("/delete/:email", expressAdaptRoute(makeDeleteUserController()));
+router.put("/update/:email", expressAdaptRoute(makeUpdateUserController()));
 
 export { router };
