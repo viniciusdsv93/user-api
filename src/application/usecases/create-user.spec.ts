a4bed12266bb6bf23971da1e526551a33ea35edb
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-12-04 01:31:24 -0300

    test: ensure CreateUser calls FindUserByEmail with the correct email

--- src/application/usecases/create-user.spec.ts
+++ src/application/usecases/create-user.spec.ts
@@ -1,13 +1,14 @@
 import { UserModel } from "../../domain/models/user";
 import { CreateUserModel } from "../../domain/usecases/create-user";
 import { IAddUserRepository } from "../protocols/add-user-respository";
+import { IFindUserByEmailRepository } from "../protocols/find-by-email-repository";
 import { CreateUser } from "./create-user";
 
 const makeFakeCreateUserModel = (): CreateUserModel => {
 	return {
 		nome: "nome_valido",
 		CPF: "CPF_valido",
-		email: "email_valido",
+		email: "email_valido@mail.com",
 		telefone: "telefone_valido",
 		sexo: "Masculino",
 		dataNascimento: "15/10/1980",
@@ -19,7 +20,7 @@ const makeFakeUserModel = (): UserModel => {
 		id: "id_valido",
 		nome: "nome_valido",
 		CPF: "CPF_valido",
-		email: "email_valido",
+		email: "email_valido@mail.com",
 		telefone: "telefone_valido",
 		sexo: "Masculino",
 		dataNascimento: new Date(1980, 10 - 1, 15),
@@ -35,21 +36,45 @@ const makeAddUserRepositoryStub = (): IAddUserRepository => {
 	return new AddUserRepositoryStub();
 };
 
+const makeFindUserByEmailRepositoryStub = (): IFindUserByEmailRepository => {
+	class FindUserByEmailRepositoryStub implements IFindUserByEmailRepository {
+		async findByEmail(email: string): Promise<UserModel | null> {
+			return new Promise((resolve) => resolve(makeFakeUserModel()));
+		}
+	}
+	return new FindUserByEmailRepositoryStub();
+};
+
 type SutTypes = {
 	sut: CreateUser;
 	addUserRepositoryStub: IAddUserRepository;
+	findUserByEmailRepositoryStub: IFindUserByEmailRepository;
 };
 
 const makeSut = (): SutTypes => {
 	const addUserRepositoryStub = makeAddUserRepositoryStub();
-	const sut = new CreateUser(addUserRepositoryStub);
+	const findUserByEmailRepositoryStub = makeFindUserByEmailRepositoryStub();
+	const sut = new CreateUser(addUserRepositoryStub, findUserByEmailRepositoryStub);
 	return {
 		sut,
 		addUserRepositoryStub,
+		findUserByEmailRepositoryStub,
 	};
 };
 
 describe("Create User Usecase", () => {
+	test("Should call FindUserByEmail with the correct email", async () => {
+		const { sut, findUserByEmailRepositoryStub } = makeSut();
+		const findUserByEmailRepositorySpy = jest.spyOn(
+			findUserByEmailRepositoryStub,
+			"findByEmail"
+		);
+		await sut.create(makeFakeCreateUserModel());
+		expect(findUserByEmailRepositorySpy).toHaveBeenCalledWith(
+			"email_valido@mail.com"
+		);
+	});
+
 	test("Should call AddUserRepository with the correct values", async () => {
 		const { sut, addUserRepositoryStub } = makeSut();
 		const addUserRepositorySpy = jest.spyOn(addUserRepositoryStub, "add");
@@ -73,7 +98,7 @@ describe("Create User Usecase", () => {
 		expect(result).toHaveProperty("id", "id_valido");
 		expect(result).toHaveProperty("nome", "nome_valido");
 		expect(result).toHaveProperty("CPF", "CPF_valido");
-		expect(result).toHaveProperty("email", "email_valido");
+		expect(result).toHaveProperty("email", "email_valido@mail.com");
 		expect(result).toHaveProperty("telefone", "telefone_valido");
 		expect(result).toHaveProperty("sexo", "Masculino");
 		expect(result).toHaveProperty("dataNascimento");
