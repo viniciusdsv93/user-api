d4460972210afa8890f27ba809b2be52be506882
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-12-04 10:46:49 -0300

    feat: ensure GetUserController calls GetUser usecase with correct email

--- src/presentation/controllers/get/get-user-controller.ts
+++ src/presentation/controllers/get/get-user-controller.ts
@@ -1,3 +1,4 @@
+import { IGetUser } from "../../../domain/usecases/get-user";
 import { InvalidParamError } from "../../errors/invalid-param-error";
 import { MissingParamError } from "../../errors/missing-param-error";
 import { badRequest, ok } from "../../helpers/http";
@@ -5,6 +6,12 @@ import { IController } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 
 export class GetUserController implements IController {
+	private readonly getUser: IGetUser;
+
+	constructor(getUser: IGetUser) {
+		this.getUser = getUser;
+	}
+
 	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
 		if (!httpRequest.params?.email) {
 			return badRequest(new MissingParamError("email"));
@@ -15,6 +22,8 @@ export class GetUserController implements IController {
 				new InvalidParamError("email", "O email informado é inválido")
 			);
 		}
+
+		await this.getUser.get(httpRequest.params.email);
 
 		return ok("");
 	}
