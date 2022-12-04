dc4f0e3afe26c43bbfa91177f3d3e47663c0e8dc
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-12-04 12:05:59 -0300

    feat: include DeleteUserController on a delete route in routes

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { expressAdaptRoute } from "../adapters/express-route-adapter";
 import { makeCreateUserController } from "../factories/create-user";
+import { makeDeleteUserController } from "../factories/delete-user";
 import { makeGetUserController } from "../factories/get-user";
 
 const router = Router();
 
 router.post("/create", expressAdaptRoute(makeCreateUserController()));
 router.get("/get/:email", expressAdaptRoute(makeGetUserController()));
+router.delete("/delete/:email", expressAdaptRoute(makeDeleteUserController()));
 
 export { router };
