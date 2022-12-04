db24ff8f9ca6daa9ef2eab12001145b3bb854363
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-12-04 13:10:12 -0300

    refactor: change update user repository parameter name

--- src/application/protocols/update-user-repository.ts
+++ src/application/protocols/update-user-repository.ts
@@ -2,5 +2,5 @@ import { UserModel } from "../../domain/models/user";
 import { CreateUserModel } from "../../domain/usecases/create-user";
 
 export interface IUpdateUserRepository {
-	update(email: string, createUserData: CreateUserModel): Promise<UserModel>;
+	update(paramsEmail: string, createUserData: CreateUserModel): Promise<UserModel>;
 }
