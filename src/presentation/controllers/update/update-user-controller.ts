f0904c78c2dba07e8001b157defa26a88c97f216
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-12-04 13:01:48 -0300

    feat: ensure UpdateUserController returns 500 if UpdateUser usecase throws

--- src/presentation/controllers/update/update-user-controller.ts
+++ src/presentation/controllers/update/update-user-controller.ts
@@ -1,7 +1,7 @@
 import { IUpdateUser } from "../../../domain/usecases/update-user";
 import { InvalidParamError } from "../../errors/invalid-param-error";
 import { MissingParamError } from "../../errors/missing-param-error";
-import { badRequest, ok } from "../../helpers/http";
+import { badRequest, ok, serverError } from "../../helpers/http";
 import { IController } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 
@@ -13,61 +13,65 @@ export class UpdateUserController implements IController {
 	}
 
 	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-		if (!httpRequest.params?.email) {
-			return badRequest(new MissingParamError("email"));
-		}
-
-		if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(httpRequest.params.email)) {
-			return badRequest(
-				new InvalidParamError("email", "O email informado é inválido")
-			);
-		}
-
-		const requiredFields = [
-			"nome",
-			"CPF",
-			"email",
-			"telefone",
-			"sexo",
-			"dataNascimento",
-		];
-
-		for (const field of requiredFields) {
-			if (!httpRequest.body[field]) {
-				return badRequest(new MissingParamError(field));
+		try {
+			if (!httpRequest.params?.email) {
+				return badRequest(new MissingParamError("email"));
 			}
+
+			if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(httpRequest.params.email)) {
+				return badRequest(
+					new InvalidParamError("email", "O email informado é inválido")
+				);
+			}
+
+			const requiredFields = [
+				"nome",
+				"CPF",
+				"email",
+				"telefone",
+				"sexo",
+				"dataNascimento",
+			];
+
+			for (const field of requiredFields) {
+				if (!httpRequest.body[field]) {
+					return badRequest(new MissingParamError(field));
+				}
+			}
+
+			if (
+				httpRequest.body["sexo"] !== "Masculino" &&
+				httpRequest.body["sexo"] !== "Feminino" &&
+				httpRequest.body["sexo"] !== "Outro"
+			) {
+				return badRequest(
+					new InvalidParamError(
+						"sexo",
+						"O sexo informado é inválido. As opções são: Masculino, Feminino ou Outro"
+					)
+				);
+			}
+
+			if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(httpRequest.body.email)) {
+				return badRequest(
+					new InvalidParamError("email", "O email informado é inválido")
+				);
+			}
+
+			const { nome, CPF, email, telefone, sexo, dataNascimento } = httpRequest.body;
+
+			await this.updateUser.update(httpRequest.params.email, {
+				nome,
+				CPF,
+				email,
+				telefone,
+				sexo,
+				dataNascimento,
+			});
+
+			return ok("");
+		} catch (error) {
+			return serverError(error as Error);
 		}
-
-		if (
-			httpRequest.body["sexo"] !== "Masculino" &&
-			httpRequest.body["sexo"] !== "Feminino" &&
-			httpRequest.body["sexo"] !== "Outro"
-		) {
-			return badRequest(
-				new InvalidParamError(
-					"sexo",
-					"O sexo informado é inválido. As opções são: Masculino, Feminino ou Outro"
-				)
-			);
-		}
-
-		if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(httpRequest.body.email)) {
-			return badRequest(
-				new InvalidParamError("email", "O email informado é inválido")
-			);
-		}
-
-		const { nome, CPF, email, telefone, sexo, dataNascimento } = httpRequest.body;
-
-		await this.updateUser.update(httpRequest.params.email, {
-			nome,
-			CPF,
-			email,
-			telefone,
-			sexo,
-			dataNascimento,
-		});
-
-		return ok("");
 	}
 }
