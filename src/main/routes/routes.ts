3500928f5a79b2e2a30841dfa1fcc0555a906a89
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-12-04 11:02:07 -0300

    feat: include GetUserController on a get route

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { expressAdaptRoute } from "../adapters/express-route-adapter";
 import { makeCreateUserController } from "../factories/create-user";
+import { makeGetUserController } from "../factories/get-user";
 
 const router = Router();
 
 router.post("/create", expressAdaptRoute(makeCreateUserController()));
+router.get("/get", expressAdaptRoute(makeGetUserController()));
 
 export { router };
